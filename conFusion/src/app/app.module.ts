e6ac26dceec5c5ff55054ba947b2fae12fa81e9e
02anagha <garg.anagha@gmail.com>
2021-11-29 18:30:21 +0530

    Assignment 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -3,22 +3,35 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material';
 
 import 'hammerjs'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	MenuComponent,
+	DishdetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    FlexLayoutModule    
+	FlexLayoutModule,
+	MatToolbarModule,
+	MatListModule,
+	MatGridListModule,
+	MatButtonModule,
+	MatCardModule
+	
   ],
   providers: [],
   bootstrap: [AppComponent]
